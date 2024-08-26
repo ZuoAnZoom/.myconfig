0edd51b003f7b156667ce9f8ab2f15ffbe357346
cuishanyao <cuishanyao@cuishanyaodeMacBook-Pro.local>
2024-08-26 15:18:44 +0800

    [feat]: update nvim config
    Signed-off-by: cuishanyao <cuishanyao@cuishanyaodeMacBook-Pro.local>

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -5,12 +5,13 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "nord",
+  transparency = false,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
 return M
